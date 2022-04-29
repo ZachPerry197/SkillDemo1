12df8ba2930f4cdef3e8df05c38d4399f185b0b6
ZachPerry197 <zperry@ucsd.edu>
2022-04-29 12:10:49 -0400

    1

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,5 +1,7 @@
 public class SkillDemo1 {
 
-
+    public static int multiply(int int1, int int2) {
+        return int1 * int2;
+    }
 
 }
---
+++ SkillDemo1Test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+    @Test
+    public void multiply() {
+        assertEquals(4, SkillDemo1.multiply(2, 3));
+    }
+
+}
